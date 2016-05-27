fe21584e26673087c28fbbd7858e5b24b67173a8
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2016-05-27 20:47:04 +0200

    permutation scala

--- src/main/scala/pl/superczynski/permutation.scala
+++ src/main/scala/pl/superczynski/permutation.scala
@@ -2,8 +2,10 @@ package main.scala.pl.superczynski
 
 object permutation {
   def main(args: Array[String]): Unit = {
+    //fast but not efficient
     def solution(A: Array[Int]): Int = {
-      if (A.length == (A.max - A.min) + 1) 1 else 0
+      if (A.length < 1 || A.length > 100000) 0
+      else if (A.length == (A.max - A.min) + 1) 1 else 0
     }
 
     println(solution(Array(4, 1, 3, 2)))
