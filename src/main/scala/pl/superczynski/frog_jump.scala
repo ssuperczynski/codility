ef8b8e6d6f4fe130ecac97936aa67e535d1b12ae
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2016-05-27 19:25:39 +0200

    to duble not to float

--- src/main/scala/pl/superczynski/frog_jump.scala
+++ src/main/scala/pl/superczynski/frog_jump.scala
@@ -6,9 +6,13 @@ package main.scala.pl.superczynski
 object frog_jump {
   def main(args: Array[String]): Unit = {
     def jump(X: Int, Y: Int, D: Int): Int = {
-      math.ceil((Y - X).toFloat / D.toFloat).toInt
+      math.ceil((Y - X).toDouble / D.toDouble).toInt
     }
 
-    println(jump(10, 85, 30))
+    println(jump(10, 85, 30)) // expected 3
+    println(jump(3, 999111321, 7), 142730189) //expected 142730189
+
+    assert(jump(10, 85, 30) == 3)
+    assert(jump(3, 999111321, 7) == 142730189)
   }
 }
