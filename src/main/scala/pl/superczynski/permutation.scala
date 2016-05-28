6fefcdb2c2bdb1f91e9c65a12e7326528cb80e54
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2016-05-28 10:47:50 +0200

    permutation efficient answer

--- src/main/scala/pl/superczynski/permutation.scala
+++ src/main/scala/pl/superczynski/permutation.scala
@@ -8,6 +8,22 @@ object permutation {
       else if (A.length == (A.max - A.min) + 1) 1 else 0
     }
 
-    println(solution(Array(4, 1, 3, 2)))
+    // efficient
+    def solution2(A: Array[Int]): Int = {
+      A.length match {
+        case x if x == 1 => 0
+        case x if x < 1 => 0
+        case x if x > 100000 => 0
+        case _ =>
+          val list = A.toList
+            .sortWith(_ < _)
+            .sliding(2)
+            .map(l => {
+              if (l(1) - l.head == 1) 1 else 0
+            })
+            .count(_ == 0)
+          if (list == 0) 1 else 0
+      }
+    }
   }
 }
